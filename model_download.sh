b528d184d081429b940c62e46f99d539efddc932
chankyupark <ckparks@gmail.com>
2023-08-03 11:03:24 +0900

    Update model_download.sh

--- model_download.sh
+++ model_download.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-/view?usp=sharing
+
 python3 gdrivedl.py https://drive.google.com/file/d/1hu3F7Ly1rEbk8L8OZCeVgZba-IYrPKXB -P fashion_att_pkg/fashion_attribute/cfg
 python3 gdrivedl.py https://drive.google.com/file/d/1kD12GEZw6nRYaqO9-1m8dairpX89z5VE -P fashion_att_pkg/fashion_attribute/cfg
